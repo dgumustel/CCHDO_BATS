f6326680cb9abdb0aba659eb6c3d73d7beb2ed7e
Derya Gumustel <yoshdragon@gmail.com>
2020-05-29 12:03:10 -0700

    add comments to data_collector.py

--- data_collector.py
+++ data_collector.py
@@ -1,8 +1,17 @@
+"""
+This code is for the final project of the 2020 Effective Computing course at UW. 
+This code scrapes BATS data from the web and saves it into a data subdirectory as .csv files. 
+This data is then put into a dictionary where the dict key is the filename and the dict value 
+is a pandas DataFrames containg data from the corresponding .csv file. 
+"""
+
 #imports
 import sys, os
 import os
 from zipfile import ZipFile
 import pandas as pd
+from bs4 import BeautifulSoup as bs
+import requests
 
 # local imports
 sys.path.append(os.path.abspath('shared'))
@@ -11,44 +20,52 @@ from importlib import reload
 reload(mymod)
 
 # make sure the output directory exists
-try:
+try:    # try for Mac OS
     this_dir = os.path.abspath('.').split('/')[-1]
     this_parent = os.path.abspath('.').split('/')[-2]
     out_dir = '../../' + this_parent + '/' + this_dir + '_data'
     print('Creating ' + out_dir + ', if needed \n')
     mymod.make_dir(out_dir)
-except:
+except:    # default to Windows OS if error occurs above
     this_dir = os.path.abspath('.').split('\\')[-1]
     this_parent = os.path.abspath('.').split('\\')[-2]
     out_dir = '../../' + this_parent + '/' + this_dir + '_data'
     print('Creating ' + out_dir + ', if needed \n')
     mymod.make_dir(out_dir)
 
-# access the data
+# identify data directory
 data_dir = '../' + this_parent + '/' + this_dir + '_data/'
 
 
-from bs4 import BeautifulSoup as bs
-import requests
 
+# DATA COLLECTION
+
+# define desired URL and filetype
 DOMAIN = 'https://cchdo.ucsd.edu'
 URL = 'https://cchdo.ucsd.edu/cruise/BIOS20160414'
 FILETYPE = '.zip'
 
+# function to get text from URL and parse as html
 def get_soup(url):
     return bs(requests.get(url).text, 'html.parser')
     
+# scrape data from URL
 for link in get_soup(URL).find_all('a'):
     file_link = link.get('href')
-    if FILETYPE in file_link:
-        print(file_link)
-        print('working')
+    if FILETYPE in file_link:    # check text for .zip files
+        print('collecting data: ' + file_link)
         with open(link.text, 'wb') as file:
-            response = requests.get(DOMAIN + file_link)
-            file.write(response.content)
+            response = requests.get(DOMAIN + file_link)    # get file
+            file.write(response.content)    # write file to machine
+            
+            # extract all files from .zip to a new data directory
             zip_file = file.name
             with ZipFile(zip_file, 'r') as zipObj:
                 zipObj.extractall(out_dir + '/' + file.name[:-8])
+
+
+
+# DATA PROCESSING
 
 # Create list of CCHDO BATS file names
 csvs = [x for x in os.listdir('../CCHDO_BATS_data/BIOS20160414/') if x.endswith('.csv')]
@@ -58,7 +75,9 @@ fns = [os.path.splitext(os.path.basename(x))[0] for x in csvs]
 d = {}
 for i in range(len(fns)):
     d[fns[i]] = pd.read_csv('../CCHDO_BATS_data/BIOS20160414/' + csvs[i], skiprows=11)
+    
 # Dictionary for the time
+
 d2 = {}
 for i in range(len(fns)):
     d2[fns[i]] = pd.read_csv('../CCHDO_BATS_data/BIOS20160414/' + csvs[i], nrows=8,usecols=[0])
@@ -66,19 +85,19 @@ for i in range(len(fns)):
 
 #############
 
+# get raw data of one file
 a = d.get('BIOS20160414_10323001_ct1')
+
+# get one column of file
 b = a['DBAR']
 
+# create empty dataframe for times
 df_ = pd.DataFrame(columns=["Time"])
 
-
 ff = pd.DataFrame.from_dict(d2,orient='index',columns=['A'])
-
-
 
 time = d2.get('BIOS20160414_10323001_ct1')
 
 #time = time['CTD'].str.extract('(\d+)').astype(int)
-#time = time.loc[5,]
 #time = time.loc[5,]
 
