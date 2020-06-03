935f662ad427c76117d58a4725dd71d47e1f715f
kimberlygotts <kimmgottschalk@gmail.com>
2020-06-02 23:19:53 -0700

    Update Plotting_BATS.py

--- Plotting_BATS.py
+++ Plotting_BATS.py
@@ -1,51 +1,54 @@
 #download
 
 # basemap: conda install -c anaconda basemap
+# basemap highres: conda install -c conda-forge basemap-data-hires
 # gsw teos10: To install with Anaconda: conda install -c conda-forge cmocean
 #remember to restart ipython after install to intialize 
 
-#imports
-import cmocean
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import gsw
-from matplotlib.ticker import MaxNLocator
-
-#make bathy map of BATS study area
-m = Basemap(width=6000000,height=5000000,projection='lcc',
-            resolution=None,lat_1=31,lat_2=33,lat_0=32,lon_0=-64.5)
-m.etopo()
-
+##make bathy map of BATS study area, Note: -180 t0 180
+print('Making Map of BATS study area')
+m = Basemap(width=1000000,height=1000000,projection='lcc',
+            resolution='h',lat_1=31,lat_2=33,lat_0=32,lon_0=-64.5)
+im = m.etopo()
+cb = m.colorbar(im,location='right',pad='10%')
 #scatter plot lons and lat coordinates on top of bathy
-x, y = m(qcdf['LONGITUDE'],qcdf['LATITUDE'])
-m.scatter(x,y,3,marker='o',color='k')
-plt.title('Locations of BATS Array’)
-
+lon_plot=lon.to_numpy() 
+lat_plot=lat.to_numpy()
+x, y = m(-lon_plot,lat_plot)
+m.scatter(x,y,c='k',s=50,marker='*')
+# meridians on bottom and left
+parallels = np.arange(0.,81,2.)
+# labels = [left,right,top,bottom]
+m.drawparallels(parallels,labels=[True,False,True,True])
+meridians = np.arange(5.,351.,2.)
+m.drawmeridians(meridians,labels=[False,False,False,True])
+plt.title('BATS Study Area')
 plt.show()
 
-##
+print('Question for the class: how do you add a colorbar to a basemap background graphics object?')
+
+
+## Needs to be updated one CTDPRS variable added to processing
 
 # PLOT 1 - T/S 
+
+print('Plotting T/S')
 
 # plot t/s
 didx=len(qcdf['CTDPRS'])
 
 qcdf = qcdf[:-didx]
-ar=qcdf.to_numpy()
-     
 
-SA=gsw.SA_from_SP(qcdf['SALNTY'],qcdf['CTDPRS'],qcdf['LONGITUDE'],qcdf['LATITUDE'])
+SA=gsw.SA_from_SP(sal,qcdf['CTDPRS'],lon_plot,lat_plot)
 
-CT=gsw.CT_from_t(SA,qcdf['CTDTMP'],qcdf['CTDPRS'])
+CT=gsw.CT_from_t(SA,temp,qcdf['CTDPRS'])
 
 for i=0:len(SA(:,1))
 CT_5mean=np.mean(CT(i,:))
 SA_5mean=np.mean(SA(i,:))
 end
 
-# plot T/S diagram - altered from example from https://medium.com@hafezahmad/making-temperature-salinity-diagrams-called-the-t-s-diagram-with-python-and-r-programming-5deec6378a29
+# plot T/S diagram - altered from example from https://medium.com@hafezahmad/making-#temperature-salinity-diagrams-called-the-t-s-diagram-with-python-and-r-#programming-5deec6378a29
 
 mint=np.min(CT_5_day_mean)
 maxt=np.max(CT_5_day_mean)
@@ -77,6 +80,7 @@ plt.show()
 #calculate MLD
 mld=gsw.mlp(SA,CT,depth)
 
+time=time.to_numpy()
 ax1=subplot(411)
 plt.contourf(time, qcdf['CTDPRS'], CT, alpha=0.7,cmap=cmocean.cm.thermal);
 Hold on
@@ -92,13 +96,20 @@ cb=plt.colorbar(SA)
 cb.set_label(‘SA’)
 
 ax3=(413,sharex=ax1)
-plt.contourf(time,qcdf['CTDPRS'],qcdf['OXY'],alpha=20, cmap=cmocean.cm.oxy)
+plt.contourf(time,qcdf['CTDPRS'],oxy,alpha=20, cmap=cmocean.cm.oxy)
 plt.setp(ax3.get_xticklabels(), visible=False)
+cb=plt.colorbar(oxy)
+cb.set_label(‘Oxygen (\mumol/kg)’)
 
 ax4=(414,sharex=ax1)
-plt.contourf(time,qcdf['CTDPRS'],qcdf['PP'],alpha=.7,cmap=cmocean.cm.algae)
+plt.contourf(time,qcdf['CTDPRS'],flo,alpha=.7,cmap=cmocean.cm.algae)
 plt.setp(ax4.get_xticklabels(), fontsize=12, visible=True)
 plt.show()
+cb=plt.colorbar(flo)
+cb.set_label(‘Fluorescence’)
+
+plt.xlabel('Time')
+plt.ylabel('Depth (dbar)')
 
 ##refs
 #https://matplotlib.org/3.2.1/api/_as_gen/matplotlib.pyplot.subplot.html
